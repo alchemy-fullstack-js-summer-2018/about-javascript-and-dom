c1c3070ef38506552ad542231e7b12d339a4f983
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-13 17:02:26 -0700

    Initial commit, file setup, and Animal test successful; had to fork and reclone

---
+++ __tests__/animal.test.js
@@ -0,0 +1,31 @@
+const Animal = require('../lib/animalClass');
+
+
+describe('Object prototypes: Two object instances...', () => {
+  
+  let frank;
+  let fido;
+
+  beforeEach(() => {
+    frank = new Animal('Frank', 'woof!');
+    fido = new Animal('Fido', 'ruff!');
+  });
+  
+  it('Have the same prototype', () => {
+    expect(frank.__proto__).toEqual(fido.__proto__);
+  });
+  
+  it('Have the same \'speak\' method', () => {
+    expect(frank.speak).toEqual(fido.speak);
+  });
+  
+  it('Have the same obj.constructor.name value of Animal', () => {
+    expect(frank.constructor.name).toEqual(fido.constructor.name);
+  });
+
+  it('Have different results from calling the \'speak\' method', () => {
+    expect(frank.speak()).toBe('Frank sez woof!');
+    expect(fido.speak()).toBe('Fido sez ruff!');
+  });
+
+});
---
+++ __tests__/variable.test.js
@@ -0,0 +1,29 @@
+
+
+describe('Variables, arguments, values, and references', () => {
+
+  it.skip('Reassigning a variable (number) changes the value of the variable', () => {
+
+  });
+
+  it.skip('Reassigning a variable (object) changes the value of the variable', () => {
+
+  });
+
+  it.skip('Assigning a value (property of object) does not change the value of the variable', () => {
+
+  });
+
+  it.skip('Passing a variable (number as argument) to function copies the value to the corresponding parameter arguments and reassigning the argument in the function will not alter the original variable value', () => {
+
+  });
+
+  it.skip('Passing a variable (object as argument) to function copies the object reference to the corresponding parameter arguments and reassigning the argument in the function will not alter the original variable', () => {
+
+  });
+
+  it.skip('Passing a variable (object as argument) to function and then assigning a property to the object inside the function will alter the original object to have the new property value', () => {
+
+  });
+
+});
