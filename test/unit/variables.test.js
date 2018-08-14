b1a109c4f18e459f282af915b3ae7ddeb3e42793
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-14 11:24:14 -0700

    Variables Test 4 & 5 Passing

--- test/unit/variables.test.js
+++ test/unit/variables.test.js
@@ -26,4 +26,28 @@ describe('Test Variables, Arguments, Values, and References', () => {
     flower.name = 'Daffodil';
     expect(flowerVar).toBe(flower);
   });
+
+  it.only('test when passing a variable that holds a number as an argument to a function copies the value to the corresponding parameter arguments and reassigning the argument in the function will not alter the original variable value.', () => {
+    var num = 2;
+    function sum(a, b) {
+      return a = a + b; 
+    } 
+    let answer = sum(num, 5);
+    expect(answer).toBe(7);
+    expect(num).toBe(2);
+  });
+
+  it('test when Passing a variable that holds an object as an argument to a function copies the object reference to the corresponding parameter arguments and reassigning the argument in the function will not alter the original variable.', () => {
+    var animal = {
+      name: 'Lion',
+      sound: 'roar',
+      number: 2
+    };
+    function sum(a, b) {
+      return a = a + b; 
+    } 
+    sum(animal.number, 5);
+    expect(animal.number).toBe(2);
+  });
+
 });  
