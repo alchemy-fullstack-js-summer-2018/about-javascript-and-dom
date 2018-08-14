c5e230e8de9419855e95189fb88b4e57e7bb8e9b
carriecarter <carrie@carrie-carter.com>
2018-08-13 17:42:45 -0700

    heading home soon

--- animal.js
+++ animal.js
@@ -1,13 +1,3 @@
 
-class Animal {
-  constructor(name, sound) {
-    this.name = name;
-    this.sound = sound;
-  }
-  
-  speak() {
-    return `${this.name} sez ${this.sound}`;
-  }
-}
 
-module.exports = Animal;
+
