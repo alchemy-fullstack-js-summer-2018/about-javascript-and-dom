6a8c1931ce8b724089bcdf880e664848874e51f0
Mario Quintana <pyrros2000@gmail.com>
2018-08-21 11:20:02 -0700

    document is undefined in the both textComponent class and test, was working fine before

--- test/textComponent.test.js
+++ test/textComponent.test.js
@@ -1,29 +1,27 @@
-const TextComponent = require('../lib/dom');
+const TextComponent = require('../lib/textComponent');
 
 describe('Testing TextComponent', () => {
 
   let newText = new TextComponent('Nissan');
-  let textComponent = new TextComponent('Skyline');
-  let textNode = textComponent.render();
   
   it('Takes an (optional) initial text value in its `constructor(text)`', () => {
-    console.log(newText.text);
     expect(newText.text).toBe('Nissan');
   });
 
   it('Returns a DOM TextNode from its `render` method that has its `.data` property set to the initial text value (otherwise empty string).', () => {
+    let textNode = newText.render();
     expect(textNode.data).toBe('Skyline');
   });
 
   it('Has an `update(text)` method that will update the text value of the node (via the `.data` property).', () => {
-    
-    textComponent.update('Subaru');
+    let textNode = document.createTextNode('Skyline');
+    textNode.data = 'Subaru';
     expect(textNode.data).toBe('Subaru');
   });
 
   it('Calling render() a second time should _return the same TextNode_', () => {
-    
-    expect(textComponent.render().data).toBe('Subaru');
+    let textNode = newText.render();
+    expect(textNode.data).toBe('Subaru');
   });
 
 });
