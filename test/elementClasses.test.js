83176967c06e65b89095427e271c3b8660cd433a
Mario Quintana <pyrros2000@gmail.com>
2018-08-21 12:33:29 -0700

    Element classes tests passing

--- test/elementClasses.test.js
+++ test/elementClasses.test.js
@@ -1,18 +1,23 @@
+const elementClasses = require('../lib/ElementClasses');
+
 describe('Testing ElementClasses', () => {
+
+  this.element = document.createElement('p');
+  let elementDom = new elementClasses(this.element);
 
   it('Takes an element in its `constructor(element)`. It should throw if argument is missing or not an instance of `HTMLElement`', () => {
    
-    expect(true).toBe(true);
+    expect(elementDom.element instanceof HTMLElement).toBeTruthy();
   });
 
   it('Has an `add(className)` method that adds the `className` to the element', () => {
-   
-    expect(true).toBe(true);
+    elementDom.add('car');
+    expect(elementDom.element.classList).toContain('car');
   });
 
   it('Has a `remove(className)` method that removes the `className` from the element (safe to call if class doesnt have that `className`)', () => {
-   
-    expect(true).toBe(true);
+    elementDom.remove('car');
+    expect(elementDom.element.classList).not.toContain('car');
   });
 
 });
